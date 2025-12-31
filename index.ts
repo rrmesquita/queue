892fcf81b2c7e19117bf27eced09fa7ce702d1a4
Romain Lanz <romain.lanz@pm.me>
2025-12-31 13:02:56 +0100

    feat: export errors and types for better DX

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 export { Job } from './src/job.js'
 export { Worker } from './src/worker.js'
 export { QueueManager } from './src/queue_manager.js'
+export { Locator } from './src/locator.js'
 export {
   customBackoff,
   linearBackoff,
   exponentialBackoff,
   fixedBackoff,
 } from './src/strategies/backoff_strategy.js'
+export * as errors from './src/exceptions.js'
---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+export type {
+  Duration,
+  JobData,
+  JobOptions,
+  JobClass,
+  RetryConfig,
+  BackoffStrategy,
+  BackoffConfig,
+  QueueConfig,
+  WorkerConfig,
+  WorkerCycle,
+  AdapterFactory,
+  QueueManagerConfig,
+} from './main.js'
+
+export type { Adapter, AcquiredJob } from '../contracts/adapter.js'
